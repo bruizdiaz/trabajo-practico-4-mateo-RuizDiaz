1ab0670637f95e42d38867e1f0b9c89aa01137ad
bruizdiaz <mateord65@gmail.com>
2025-06-24 09:14:25 -0300

    Comienzo de los controladores y rutas

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import { initDB } from './src/config/database.js';
+import characterRoutes from './src/routes/character.routes.js';
 
 const app = express();
 const PORT = Number(process.env.PORT);
 
 app.use(express.json);
 app.use(morgan('dev'));
+app.use(characterRoutes);
 
 initDB();
 app.listen(PORT, console.log(`Servidor escuchando en http://localhost:${PORT}`));
--- src/controllers/character.controllers.js
+++ src/controllers/character.controllers.js
@@ -1,3 +1,18 @@
 //------------------------------------//
 //------------- IMPORT --------------//
 //----------------------------------//
+import express from 'express';
+import { Character } from '../models/character.model.js';
+
+export const getCharacters = async (req, res) => {
+	try {
+		const characters = await Character.findAll();
+		res.json(characters);
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const getCharacterByID = async (req, res) => {};
+export const createCharacter = async (req, res) => {};
+export const updateCharacter = async (req, res) => {};
+export const deleteCharacter = async (req, res) => {};
